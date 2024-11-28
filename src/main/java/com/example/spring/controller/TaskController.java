979a79899e80e0a3ba92566d11b8dccd90fd307f
DmitriiMaikov <dmitriimaikov15@gmail.com>
2024-11-28 10:11:51 +0300

    first commit

---
+++ src/main/java/com/example/spring/controller/TaskController.java
@@ -0,0 +1,52 @@
+package com.example.spring.controller;
+
+import com.example.spring.model.Task;
+import com.example.spring.repository.TaskRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+public class TaskController {
+
+    private final TaskRepository taskRepository;
+
+    @PostMapping("/tasks")
+    public Task create(@RequestBody Task task) {
+        return taskRepository.save(task);
+    }
+
+    @GetMapping("/tasks")
+    public Iterable<Task> findAll() {
+        return taskRepository.findAll();
+    }
+
+    @GetMapping("/tasks/{id}")
+    public Task findById(@PathVariable Long id) {
+        return taskRepository.findById(id).orElse(null);
+    }
+
+    @PutMapping("/tasks/{id}")
+    public Task update(@PathVariable Long id, @RequestBody Task task) {
+        task.setId(id);
+        return taskRepository.save(task);
+    }
+
+    @DeleteMapping("/tasks/{id}")
+    public void delete(@PathVariable Long id) {
+        Task task = taskRepository.findById(id).get();
+        taskRepository.delete(task);
+    }
+
+    @PatchMapping("/tasks/{id}")
+    public void patchMethod(@PathVariable Long id,
+                            @RequestBody Task task){
+        task.setId(id);
+        taskRepository.save(task);
+    }
+
+    @PatchMapping("/tasks/{id}:mark-as-done")
+    public void patchMethod(@PathVariable Long id){
+        taskRepository.markAsDone(id);
+    }
+}
